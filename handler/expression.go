f04ad033acecdd5468ed2212e0d5bb9c584ab255
victor lisboa <victor.lisboa@bemobi.com.br>
2023-01-22 10:55:12 -0300

    creating handler

---
+++ handler/expression.go
@@ -0,0 +1,150 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+	"github.com/viclisboa/regularExpressionEvaluatorAPI/repository"
+	"github.com/viclisboa/regularExpressionEvaluatorAPI/service"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+type ExpressionHandler struct {
+	ExpressionService    service.ExpressionService
+	ExpressionRepository repository.ExpressionInterface
+	Logger               log.Logger
+}
+
+func (eh *ExpressionHandler) EvaluateExpression(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := GetURLParams(r)
+	expressionId := params["expressionId"]
+	logger := eh.Logger.WithField("expressionId", expressionId)
+
+	expressionIdAsInt, err := strconv.Atoi(expressionId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		logger.WithField("err", err.Error()).Error("error parsing expressionId to int")
+		return
+	}
+
+	expression, err := eh.ExpressionRepository.GetExpressionById(expressionIdAsInt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		logger.WithField("err", err.Error()).Error("error recovering expression from database")
+		return
+	}
+
+	result, err := eh.ExpressionService.ExecuteExpression(expression, r.URL.RawQuery)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		logger.WithField("err", err.Error()).Error("error resolving expression")
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		http.Error(w, "Error on marshal ", http.StatusInternalServerError)
+		logger.WithField("err", err.Error()).Error("error encoding response")
+		return
+	}
+}
+
+func (eh *ExpressionHandler) SaveExpression(w http.ResponseWriter, r *http.Request) {
+	params := GetURLParams(r)
+	expressionId := params["expressionId"]
+
+	logger := eh.Logger.WithField("expressionId", expressionId)
+
+	expressionIdAsInt, err := strconv.Atoi(expressionId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		logger.WithField("err", err.Error()).Error("error parsing expressionId to int")
+		return
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		logger.WithField("err", err.Error()).Error("Error on get body data for expression update")
+		http.Error(w, "Error on get body data for expression update", http.StatusInternalServerError)
+		return
+	}
+
+	var body map[string]any
+	err = json.Unmarshal(b, &body)
+	if err != nil {
+		logger.WithField("err", err.Error()).Error("Error on unmarshal  payload for update")
+		http.Error(w, "Error on unmarshal landing page payload for creation", http.StatusInternalServerError)
+		return
+	}
+
+	definition, exists := body["definition"]
+	if definition == "" || !exists {
+		logger.WithField("body", body).Error("missing definition on body")
+		http.Error(w, "missing definition in body", http.StatusBadRequest)
+		return
+	}
+
+	definitionAsString := definition.(string)
+
+	err = eh.ExpressionRepository.SaveExpression(expressionIdAsInt, definitionAsString)
+	if err != nil {
+		logger.WithField("err", err.Error()).Error("Error updating expressiong")
+		http.Error(w, "Error updating expression", http.StatusInternalServerError)
+		return
+	}
+
+	logger.Info("expression updated successfully")
+}
+
+func (eh *ExpressionHandler) CreateExpression(w http.ResponseWriter, r *http.Request) {
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		eh.Logger.WithField("err", err.Error()).Error("Error on get body data for expression update")
+		http.Error(w, "Error on get body data for expression update", http.StatusInternalServerError)
+		return
+	}
+
+	var body map[string]any
+	err = json.Unmarshal(b, &body)
+	if err != nil {
+		eh.Logger.WithField("err", err.Error()).Error("Error on unmarshal  payload for update")
+		http.Error(w, "Error on unmarshal landing page payload for creation", http.StatusInternalServerError)
+		return
+	}
+
+	definition, exists := body["definition"].(string)
+	if definition == "" || !exists {
+		eh.Logger.WithField("body", body).Error("missing definition on body")
+		http.Error(w, "missing definition in body", http.StatusBadRequest)
+		return
+	}
+
+	err = eh.ExpressionRepository.CreateExpression(definition)
+	if err != nil {
+		eh.Logger.WithField("err", err.Error()).Error("Error updating expressiong")
+		http.Error(w, "Error updating expression", http.StatusInternalServerError)
+		return
+	}
+
+	eh.Logger.Info("expression created successfully")
+}
+
+func GetURLParams(r *http.Request) map[string]string {
+	rctx := chi.RouteContext(r.Context())
+	var urlParams map[string]string
+	for k := len(rctx.URLParams.Keys) - 1; k >= 0; k-- {
+		switch urlParams {
+		case nil:
+			urlParams = make(map[string]string)
+			fallthrough
+		default:
+			if rctx.URLParams.Keys[k] != "*" {
+				urlParams[rctx.URLParams.Keys[k]] = rctx.URLParams.Values[k]
+			}
+		}
+	}
+	return urlParams
+}
---
+++ handler/expression_test.go
@@ -0,0 +1,203 @@
+package handler
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/viclisboa/regularExpressionEvaluatorAPI/model"
+	"github.com/viclisboa/regularExpressionEvaluatorAPI/repository"
+	"github.com/viclisboa/regularExpressionEvaluatorAPI/service"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestSaveExpression(t *testing.T) {
+	testCases := []struct {
+		name         string
+		databaseMock repository.Stub
+		requestBody  map[string]any
+		httpStatus   int
+	}{
+		{
+			name: "should return 200",
+			databaseMock: repository.Stub{
+				SaveExpressionError: nil,
+			},
+			requestBody: map[string]any{
+				"definition": "a or b",
+			},
+			httpStatus: http.StatusOK,
+		},
+		{
+			name:         "should return 400, missing definition in json",
+			databaseMock: repository.Stub{},
+			requestBody: map[string]any{
+				"sdsadasd": "dasdsa",
+			},
+			httpStatus: http.StatusBadRequest,
+		},
+		{
+			name: "should return 500, error saving in database",
+			databaseMock: repository.Stub{
+				SaveExpressionError: errors.New("not found"),
+			},
+			requestBody: map[string]any{
+				"definition": "dasdsa",
+			},
+			httpStatus: http.StatusInternalServerError,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			handler := ExpressionHandler{
+				ExpressionService:    service.ExpressionService{},
+				ExpressionRepository: &tc.databaseMock,
+				Logger:               log.Logger{},
+			}
+
+			r := chi.NewRouter()
+			r.HandleFunc("/expressions/{expressionId}", handler.SaveExpression)
+			ts := httptest.NewServer(r)
+			defer ts.Close()
+
+			url := ts.URL + "/expressions/1"
+
+			var buf bytes.Buffer
+			_ = json.NewEncoder(&buf).Encode(tc.requestBody)
+
+			response, _ := http.Post(url, "application/json", &buf)
+
+			assert.Equal(t, tc.httpStatus, response.StatusCode)
+		})
+	}
+}
+
+func TestCreateExpression(t *testing.T) {
+	testCases := []struct {
+		name         string
+		databaseMock repository.Stub
+		requestBody  map[string]any
+		httpStatus   int
+	}{
+		{
+			name: "should return 200",
+			databaseMock: repository.Stub{
+				CreateExpressionError: nil,
+			},
+			requestBody: map[string]any{
+				"definition": "a or b",
+			},
+			httpStatus: http.StatusOK,
+		},
+		{
+			name:         "should return 400, missing definition in json",
+			databaseMock: repository.Stub{},
+			requestBody: map[string]any{
+				"sdsadasd": "dasdsa",
+			},
+			httpStatus: http.StatusBadRequest,
+		},
+		{
+			name: "should return 500, error saving in database",
+			databaseMock: repository.Stub{
+				CreateExpressionError: errors.New("not found"),
+			},
+			requestBody: map[string]any{
+				"definition": "dasdsa",
+			},
+			httpStatus: http.StatusInternalServerError,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			handler := ExpressionHandler{
+				ExpressionService:    service.ExpressionService{},
+				ExpressionRepository: &tc.databaseMock,
+				Logger:               log.Logger{},
+			}
+
+			r := chi.NewRouter()
+			r.HandleFunc("/expressions", handler.CreateExpression)
+			ts := httptest.NewServer(r)
+			defer ts.Close()
+
+			url := ts.URL + "/expressions"
+
+			var buf bytes.Buffer
+			_ = json.NewEncoder(&buf).Encode(tc.requestBody)
+
+			response, _ := http.Post(url, "application/json", &buf)
+
+			assert.Equal(t, tc.httpStatus, response.StatusCode)
+		})
+	}
+}
+
+func TestEvaluateExpression(t *testing.T) {
+	testCases := []struct {
+		name         string
+		databaseMock repository.Stub
+		httpStatus   int
+		expectedBody model.Response
+		queryString  string
+	}{
+		{
+			name: "should return 200",
+			databaseMock: repository.Stub{
+				GetExpressionByIdResponse: model.Expression{
+					ID:         10,
+					Definition: "x or y",
+				},
+				GetExpressionByIdError: nil,
+			},
+			httpStatus: http.StatusOK,
+			expectedBody: model.Response{
+				Definition: "x or y",
+				Values:     "x=1,y=0",
+				Result:     true,
+			},
+			queryString: "?x=1,y=0",
+		},
+		{
+			name: "should return 404, expression not found",
+			databaseMock: repository.Stub{
+				GetExpressionByIdError: errors.New("not found"),
+			},
+			httpStatus:   http.StatusNotFound,
+			expectedBody: model.Response{},
+			queryString:  "?x=1,y=0,z=1",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			handler := ExpressionHandler{
+				ExpressionService:    service.ExpressionService{},
+				ExpressionRepository: &tc.databaseMock,
+				Logger:               log.Logger{},
+			}
+
+			r := chi.NewRouter()
+			r.HandleFunc("/expressions/{expressionId}", handler.EvaluateExpression)
+			ts := httptest.NewServer(r)
+			defer ts.Close()
+
+			url := ts.URL + "/expressions/10" + tc.queryString
+
+			response, _ := http.Get(url)
+
+			var parsedResponse model.Response
+
+			_ = json.NewDecoder(response.Body).Decode(&parsedResponse)
+
+			assert.Equal(t, tc.httpStatus, response.StatusCode)
+			assert.Equal(t, tc.expectedBody, parsedResponse)
+		})
+	}
+}
