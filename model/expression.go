ebf1b83ab1df98a243fcbdb65d0e78334cf550f7
victor lisboa <victor.lisboa@bemobi.com.br>
2023-01-22 09:54:39 -0300

    adding stub for testing

--- model/expression.go
+++ model/expression.go
@@ -4,3 +4,9 @@ type Expression struct {
 	ID         int    `gorm:"column:id" json:"id"`
 	Definition string `gorm:"column:definition" json:"definition"`
 }
+
+type Response struct {
+	Definition string `json:"definition"`
+	Values     string `json:"values"`
+	Result     bool   `json:"result"`
+}
